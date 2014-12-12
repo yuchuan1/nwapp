054219264666f82c99bcf71eca3de20e09a9d3c4
Eddie Chen <yuchuan1@gmail.com>
2014-12-12 15:29:42 +0800

    test class

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
 var binding = require('pre-gyp-find')('app1');
-
-var Hello = function()
-{
-	binding.hello();
-}
-module.exports = Hello;
+var Apple = require('./lib/apple');
 console.log(binding.hello() + " from node-pre-gyp-test-app1");
